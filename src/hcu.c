5b668ce678726b1ca7769fe80ea9dd828acd1af3
Haydn <haydnt@zohomail.com>
2023-07-11 23:16:31 -0700

    began functions to get data, also changed the programdata struct

--- src/hcu.c
+++ src/hcu.c
@@ -21,7 +21,7 @@
 #include <stdlib.h> 
 #include <stddef.h>
 #include <dirent.h> 
-
+#define MAX_NAME_LENGTH  256
 
 /*  
  *  :::::::::::::ProgramData:::::::::::::::
@@ -35,8 +35,8 @@ typedef
 struct 
 {
     unsigned int pid;
-    char percentage[4];
-    char process_name[];
+    unsigned int perc;
+    char process_name[MAX_NAME_LENGTH];
 } ProgramData;
 
 
@@ -49,18 +49,68 @@ PrintHelp()
 {}
 
 
+
+
+/*
+ * ::::::::::::_procCpuData::::::::::::::
+ *  Given a non-null ProgramData pointer, this function will assign the pid, cpu %, and process name of the process with the highest CPU usage.
+ *  to the ProgramData pointer
+ *  
+ *  To get this data, the function shall read each PID directory in /proc, while updating the pid, cpu % and process_name in O(n) time.
+ *
+ *
+ */
+void 
+_procCpuData(ProgramData *data) 
+{ 
+	DIR *proc = opendir("/proc");
+	
+	struct dirent* dirData;
+
+	while((dirData = readdir(proc)) != NULL) {
+	
+	//filter out ./ and ../ otherwise we will go out of /proc and into some other directory, that would be weird if that happened.
+
+	}
+
+
+}
+
+
+
+
+
 /*
  * ::::::::::::getHighestProcess()::::::::::::::
  * This function will return a ProgramData pointer with information about the process using the highest amount of CPU.
  * If there is any issue getting the data, this function will return NULL. As a result, the function also assumes 
  * the client code will handle any NULL returned.
+ *
  */
-ProgramData *getHighestProcess()
+ProgramData*
+getHighestProcess()
 {
-return NULL;
+	ProgramData *data = malloc (sizeof(ProgramData));
+	data->pid = -1;
+	data->perc= 0;
+	data->process_name[0] = '\0';
+	_procCpuData(data);
+
+
+	return data;
 }
 
 
 int 
 main(int argc, char *argv[]) 
-{ return 0; }
+{ 
+	
+ 	ProgramData *data = getHighestProcess();
+	
+	printf("%s", data->process_name);
+	free(data);
+	return 0; 
+
+
+
+}
