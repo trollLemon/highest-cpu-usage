064df7f4830b43863f1c59c9c752b02a80996473
Haydn <haydnt@zohomail.com>
2023-07-12 18:38:24 -0700

    gets process name, though needs work

--- src/hcu.c
+++ src/hcu.c
@@ -20,22 +20,35 @@
 #include <stdio.h>   
 #include <stdlib.h> 
 #include <stddef.h>
-#include <dirent.h> 
-#define MAX_NAME_LENGTH  256
+#include <dirent.h>
+#include <string.h>
+#define MAX_NAME_LENGTH  257
+
+
+
+
+
+
+
+
+
+/*TODO: get % of cpu a process is using*/
+
+
 
 /*  
  *  :::::::::::::ProgramData:::::::::::::::
  *  This struct will hold relevant program data.
  *  Pid: this is the process id.
  *  percentage: this is the percentage of the CPU the process is using. 
- *  process_name: flexible array of chars so we can account for variable-sized process names.
+ *  process_name: array of chars for process names.
  *
  */
 typedef 
 struct 
 {
     unsigned int pid;
-    unsigned int perc;
+    unsigned long perc;
     char process_name[MAX_NAME_LENGTH];
 } ProgramData;
 
@@ -56,7 +69,7 @@ PrintHelp()
  *  Given a non-null ProgramData pointer, this function will assign the pid, cpu %, and process name of the process with the highest CPU usage.
  *  to the ProgramData pointer
  *  
- *  To get this data, the function shall read each PID directory in /proc, while updating the pid, cpu % and process_name in O(n) time.
+ *  To get this data, the function shall read each PID directory in /proc, while updating the pid, cpu  and process_name in O(n) time.
  *
  *
  */
@@ -67,9 +80,41 @@ _procCpuData(ProgramData *data)
 	
 	struct dirent* dirData;
 
-	while((dirData = readdir(proc)) != NULL) {
+	while((dirData = readdir(proc)) != NULL)
+	{
 	
 	//filter out ./ and ../ otherwise we will go out of /proc and into some other directory, that would be weird if that happened.
+	//also make sure we are looking at another directory and dont treat a text file like one
+	if (dirData->d_type == DT_DIR && strcmp(dirData->d_name, "..") != 0 && strcmp(dirData->d_name, ".") != 0)
+	{
+		
+	    //get the path to the stat file of the pid dir.
+	    char path[300];
+            snprintf(path, sizeof(path), "/proc/%s/stat", dirData->d_name);
+            FILE* file = fopen(path, "r");
+	    if (file != NULL)
+	    {
+	    
+		    int pid = 0;
+		    char process_name[MAX_NAME_LENGTH];
+		    unsigned long user_time = 0;
+		    unsigned long system_time = 0;
+	    	    fscanf(file, "%d %s %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %lu %lu",
+       			&pid, process_name, &user_time, &system_time);
+	    	
+		    //if we found a process with more cpu, update the data pointer
+		    if(user_time + system_time > data->perc){
+			
+			    data->pid = pid;
+			    data->perc= user_time+system_time;
+			    strcpy(data->process_name, process_name);
+
+		    }
+		    
+
+	    }
+	
+	}
 
 	}
 
